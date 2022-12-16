ec5f5dac2d7e82c9e006764fe77d9281b169ed3d
Ka Chun <34387770+kachun333@users.noreply.github.com>
2022-12-16 08:56:44 +0800

    [Refactor] FirebaseProvider

--- src/providers/firebase/FirebaseContext.tsx
+++ src/providers/firebase/FirebaseContext.tsx
@@ -1,10 +1,21 @@
-import { FirebaseApp } from "firebase/app";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
 import { createContext } from "react";
+import { FIREBASE_CONFIG } from "./firebase.constants";
 
 export interface FirebaseContextValue {
-  app?: FirebaseApp;
+  app: FirebaseApp;
+  auth: Auth;
 }
 
-const FirebaseContext = createContext<FirebaseContextValue>({});
+const app = initializeApp(FIREBASE_CONFIG);
+const auth = getAuth(app);
+export const firebaseContextValue: FirebaseContextValue = {
+  app,
+  auth,
+};
+
+const FirebaseContext =
+  createContext<FirebaseContextValue>(firebaseContextValue);
 
 export default FirebaseContext;
--- src/providers/firebase/FirebaseProvider.tsx
+++ src/providers/firebase/FirebaseProvider.tsx
@@ -1,15 +1,7 @@
-import { initializeApp } from "firebase/app";
-import React, { PropsWithChildren, useMemo } from "react";
-import { FIREBASE_CONFIG } from "./firebase.constants";
-import FirebaseContext, { FirebaseContextValue } from "./FirebaseContext";
+import React, { PropsWithChildren } from "react";
+import FirebaseContext, { firebaseContextValue } from "./FirebaseContext";
 
 const FirebaseProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
-  const firebaseContextValue: FirebaseContextValue = useMemo(() => {
-    return {
-      app: initializeApp(FIREBASE_CONFIG),
-    };
-  }, []);
-
   return (
     <FirebaseContext.Provider value={firebaseContextValue}>
       {children}
