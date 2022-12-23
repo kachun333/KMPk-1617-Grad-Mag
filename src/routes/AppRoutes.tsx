3c36f2b36f53632135af10e833e8fda94c0d94af
Ka Chun <34387770+kachun333@users.noreply.github.com>
2022-12-23 20:22:41 +0800

    [Refactor] remove auth pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,3 @@
-import Create from "components/pages/auth/Create";
-import Login from "components/pages/auth/Login";
-import Reset from "components/pages/auth/Reset";
-import Verify from "components/pages/auth/Verify";
 import GraduateDetails from "components/pages/GraduateDetails";
 import Graduates from "components/pages/graduates/Graduates";
 import Home from "components/pages/Home";
@@ -15,12 +11,6 @@ const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="auth" element={<Home />}>
-        <Route path="login" element={<Login />} />
-        <Route path="verify" element={<Verify />} />
-        <Route path="create" element={<Create />} />
-        <Route path="reset" element={<Reset />} />
-      </Route>
       <Route path="graduates" element={<Graduates />}>
         <Route path=":id" element={<GraduateDetails />} />
       </Route>
