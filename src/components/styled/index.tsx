e72ab0afa32be694009cd7324e8d6f5203b3b53f
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-01-26 16:11:54 +0800

    [Code] create src\components\styled\index.tsx

---
+++ src/components/styled/index.tsx
@@ -0,0 +1,7 @@
+import { styled } from "@mui/material/styles";
+import { Link as ReactRouterLink } from "react-router-dom";
+
+export const Link = styled(ReactRouterLink)({
+  textDecoration: "none",
+  color: "inherit",
+});
