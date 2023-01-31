50a5c601cf924abb7acc4fc9f0db7058484d468d
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-01-31 21:42:18 +0800

    [Code] GridLayout css

--- src/pages/graduates/GridLayout/index.styled.ts
+++ src/pages/graduates/GridLayout/index.styled.ts
@@ -1,8 +1,10 @@
 import { styled } from "@mui/material/styles";
 
 export const GridContainer = styled("div")(({ theme }) => ({
+  marginTop: 20,
   display: "flex",
   flexWrap: "wrap",
+  justifyContent: "center",
   [theme.breakpoints.down("sm")]: {
     flexDirection: "column",
   },
