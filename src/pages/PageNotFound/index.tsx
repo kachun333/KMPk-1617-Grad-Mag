60a2f751b0a38c9f47c48436abdc719bcbfda27a
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-02-05 21:20:21 +0800

    [Refactor] PageNotFound css

--- src/pages/PageNotFound/index.tsx
+++ src/pages/PageNotFound/index.tsx
@@ -3,25 +3,13 @@ import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import React from "react";
 
-const PREFIX = "Login";
-
-const classes = {
-  container: `${PREFIX}-container`,
-};
-
-const StyledContainer = styled(Container)(() => ({
-  [`&.${classes.container}`]: {
-    display: "flex",
-    flexDirection: "column",
-    // full screen - appbar height - marginTop - footer MarginTop - footer height
-    minHeight: "calc(100vh - 64px - 20px - 256px - 64px)",
-    marginTop: "20px",
-  },
-}));
+const StyledContainer = styled(Container)({
+  marginTop: "20px",
+});
 
 function PageNotFound() {
   return (
-    <StyledContainer className={classes.container}>
+    <StyledContainer>
       <Typography variant="h4">Error 404</Typography>
       <Typography variant="subtitle1">
         The page you are looking for is not found
