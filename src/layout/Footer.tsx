2d942563b43a792c396f8900f1b3e13722d24959
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-02-05 14:16:26 +0800

    [Code] rebrand to 醇忆 Grad Mag

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -78,7 +78,7 @@ function Footer() {
       <div className={classes.overlay} />
       <Box id="footer-content" className={classes.box}>
         <Typography variant="h6" gutterBottom>
-          三年之约 Our Promise
+          醇忆 Grad Mag
         </Typography>
         <Link
           href="mailto: gmagazinekmpk@gmail.com"
