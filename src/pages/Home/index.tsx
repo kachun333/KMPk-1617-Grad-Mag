58046617119668b07f386dcfdc1835d5e0c5f6fa
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-02-11 12:46:40 +0800

    [Code] Home uses FeaturedContent

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,9 @@
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActionArea from "@mui/material/CardActionArea";
-import CardMedia from "@mui/material/CardMedia";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import GraduatesImage from "assets/images/banner/graduates.webp";
-import VideosImage from "assets/images/banner/videos.webp";
 import React from "react";
-import { Link } from "react-router-dom";
+import FeaturedContent from "./FeaturedContent";
 import * as S from "./index.styled";
 
 const PREFIX = "Home";
@@ -22,17 +16,10 @@ const classes = {
   card: `${PREFIX}-card`,
   cardMedia: `${PREFIX}-cardMedia`,
   cardContent: `${PREFIX}-cardContent`,
-  overlay: `${PREFIX}-overlay`,
 };
 
 // TODO jss-to-styled codemod: The Fragment root was replaced by div. Change the tag if needed.
 const Root = styled("div")(({ theme }) => ({
-  [`& .${classes.container}`]: {
-    display: "flex",
-    flexDirection: "column",
-    flex: 1,
-  },
-
   [`& .${classes.section}`]: {
     margin: `${theme.spacing(2)} 0px`,
   },
@@ -45,45 +32,7 @@ const Root = styled("div")(({ theme }) => ({
   [`& .${classes.paragraph}`]: {
     margin: theme.spacing(2),
   },
-
-  [`& .${classes.card}`]: {
-    position: "relative",
-  },
-
-  [`& .${classes.cardMedia}`]: {
-    height: "280px",
-  },
-
-  [`& .${classes.cardContent}`]: {
-    position: "absolute",
-    bottom: 0,
-    margin: "0 32px",
-    marginBottom: "8px",
-  },
-
-  [`& .${classes.overlay}`]: {
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    left: 0,
-    paddingBottom: "100px",
-    backgroundImage: `linear-gradient(to bottom, transparent, ${theme.palette.background.paper})`,
-    opacity: 0.5,
-  },
 }));
-
-const cards = [
-  {
-    title: "Graduates",
-    link: "graduates",
-    image: GraduatesImage,
-  },
-  {
-    title: "KMPk TV",
-    link: "videos",
-    image: VideosImage,
-  },
-];
 
 function Home() {
   return (
@@ -96,8 +45,8 @@ function Home() {
           }}
         />
       </S.HeroContainer>
-      <Container className={classes.container}>
-        <Box id="about" className={classes.section}>
+      <Container>
+        <Box className={classes.section}>
           <Typography variant="h3" className={classes.paragraph}>
             About
           </Typography>
@@ -110,26 +59,7 @@ function Home() {
             Stay tune for more interesting content and interactive features!
           </Typography>
         </Box>
-        <Box id="content-cards" className={classes.section}>
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card.title} xs={12} md={6}>
-                <Typography variant="subtitle1">{card.title}</Typography>
-                <Card>
-                  <CardActionArea className={classes.card}>
-                    <Link to={card.link}>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image={card.image}
-                        title={card.title}
-                      />
-                    </Link>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
+        <FeaturedContent />
       </Container>
     </Root>
   );
