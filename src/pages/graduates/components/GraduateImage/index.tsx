084170eb5bf5ae97ab3f77e065de7e37d9629604
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-02-11 11:52:52 +0800

    [Code] GraduateImage use CloudResponsiveImg

--- src/pages/graduates/components/GraduateImage/index.tsx
+++ src/pages/graduates/components/GraduateImage/index.tsx
@@ -1,3 +1,4 @@
+import CloudResponsiveImg from "components/common/CloudResponsiveImg";
 import React from "react";
 
 interface GraduateImageProps {
@@ -14,22 +15,12 @@ const GraduateImage: React.FC<GraduateImageProps> = ({
   className,
   imgProps,
 }) => {
-  const uriEncodedGraduateName = encodeURI(graduateName);
-  const hostName =
-    "https://storage.googleapis.com/ourpromise.appspot.com/webp/graduates";
   return (
-    <img
+    <CloudResponsiveImg
+      folderPath="webp/graduates"
+      fileName={graduateName}
       className={className}
-      src={`${hostName}/300x200/${uriEncodedGraduateName}.webp`}
-      srcSet={`
-        ${hostName}/300x200/${uriEncodedGraduateName}.webp 300w,
-        ${hostName}/600x400/${uriEncodedGraduateName}.webp 600w,
-        ${hostName}/900x600/${uriEncodedGraduateName}.webp 900w,
-        ${hostName}/1200x800/${uriEncodedGraduateName}.webp 1200w,
-        ${hostName}/2400x1600/${uriEncodedGraduateName}.webp 2400w
-      `}
-      alt={graduateName}
-      {...imgProps}
+      imgProps={imgProps}
     />
   );
 };
