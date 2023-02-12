83418cf68cfe113dffbcd212665f9ac78a1f56a8
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-02-12 12:20:37 +0800

    [Code] scroll animation when FAB exists

--- src/layout/ServiceWorker/index.styled.ts
+++ src/layout/ServiceWorker/index.styled.ts
@@ -2,25 +2,35 @@ import { Avatar, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 
+const initialBottom = 32;
 const FABHeight = 56;
-export const StyledPaper = styled(Paper)(({ theme }) => ({
-  position: "fixed",
-  bottom: theme.spacing(3),
-  left: "50%",
-  transform: "translateX(-50%)",
-  padding: `${theme.spacing(1.5)} ${theme.spacing(2)}`,
-  width: 320,
-  cursor: "pointer",
+const marginTopFAB = 8;
+const translateY = marginTopFAB + FABHeight;
 
-  [theme.breakpoints.only("xs")]: {
-    bottom: 20 + FABHeight + 20,
-    width: "96%",
-  },
-}));
+export const StyledPaper = styled(Paper, {
+  shouldForwardProp: (prop) => prop !== "hasFAB",
+})<{ hasFAB: boolean }>(({ theme, hasFAB }) => {
+  const styleWithFAB = hasFAB && {
+    transform: `translate(-50%, -${translateY}px)`,
+    transition: `transform ${theme.transitions.duration.leavingScreen}ms ${theme.transitions.easing.sharp}`,
+  };
 
-export const StyledAvatar = styled(Avatar)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-}));
+  return {
+    cursor: "pointer",
+    width: 320,
+    padding: `${theme.spacing(1.5)} ${theme.spacing(2)}`,
+    position: "fixed",
+    bottom: initialBottom,
+    left: "50%",
+    transform: "translateX(-50%)",
+    transition: `transform ${theme.transitions.duration.enteringScreen}ms ${theme.transitions.easing.sharp}`,
+
+    [theme.breakpoints.down("sm")]: {
+      width: "95%",
+      ...styleWithFAB,
+    },
+  };
+});
 
 export const StyledContainer = styled("div")({
   display: "flex",
@@ -28,7 +38,15 @@ export const StyledContainer = styled("div")({
   alignItems: "center",
 });
 
+export const StyledTextContainer = styled("div")({
+  flex: 1,
+});
+
 export const StyledCaption = styled(Typography)({
   /* so that caption is nicely align */
   marginTop: -4,
 });
+
+export const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+}));
--- src/layout/ServiceWorker/index.tsx
+++ src/layout/ServiceWorker/index.tsx
@@ -1,23 +1,25 @@
 import { Typography } from "@mui/material";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 import React from "react";
 import useServiceWorker from "./hooks/useServiceWorker";
 import { HAS_UPDATE_SNACKBAR, UPDATING_SNACKBAR } from "./index.constants";
 import * as S from "./index.styled";
 
 const ServiceWorker: React.FC = () => {
+  const trigger = useScrollTrigger();
   const { isLoading, hasUpdate, updateApp } = useServiceWorker();
 
   if (!hasUpdate) return null;
   const snackbarObj = isLoading ? UPDATING_SNACKBAR : HAS_UPDATE_SNACKBAR;
   return (
-    <S.StyledPaper onClick={updateApp} elevation={10}>
+    <S.StyledPaper hasFAB={trigger} onClick={updateApp} elevation={10}>
       <S.StyledContainer>
-        <div>
+        <S.StyledTextContainer>
           <Typography variant="h6">{snackbarObj.title}</Typography>
           <Typography variant="caption" component="div">
             {snackbarObj.caption}
           </Typography>
-        </div>
+        </S.StyledTextContainer>
         {snackbarObj.avatar}
       </S.StyledContainer>
     </S.StyledPaper>
