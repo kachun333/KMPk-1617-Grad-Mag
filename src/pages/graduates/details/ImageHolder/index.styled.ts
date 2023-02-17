b88c97c0dfd3c1d11101b01e00426f40b953f4d0
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-02-17 08:15:49 +0800

    [Code] ImageHolder toolbar add ShareButton

--- src/pages/graduates/details/ImageHolder/index.styled.ts
+++ src/pages/graduates/details/ImageHolder/index.styled.ts
@@ -1,13 +1,24 @@
+import { IconButton } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
 import GraduateImage from "pages/graduates/components/GraduateImage";
 
 export const ImageToolbar = styled(Toolbar)({
+  background: "linear-gradient(0deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.4))",
   position: "absolute",
   top: 0,
   left: 0,
   right: 0,
+  display: "flex",
 });
+
+export const ImageToolbarTitle = styled("div")({
+  flex: 1,
+});
+
+export const ImageToolbarIconButton = styled(IconButton)(({ theme }) => ({
+  color: theme.palette.common.white,
+}));
 
 export const StyledGraduateImage = styled(GraduateImage)(({ theme }) => ({
   alignItems: "center",
