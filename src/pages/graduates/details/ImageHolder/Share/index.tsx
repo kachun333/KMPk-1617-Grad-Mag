3ead0367022e3012f1f886e0368c5077c4af7e7b
Ka Chun <34387770+kachun333@users.noreply.github.com>
2023-02-17 08:12:36 +0800

    [Code] ImageHolder share button

---
+++ src/pages/graduates/details/ImageHolder/Share/index.tsx
@@ -0,0 +1,33 @@
+import Share from "@mui/icons-material/Share";
+import { APP_NAME } from "global.constants";
+import * as S from "pages/graduates/details/ImageHolder/index.styled";
+import React from "react";
+
+interface ShareProps {
+  graduateUrl: string;
+  graduateText: string;
+}
+
+const ShareButton: React.FC<ShareProps> = ({ graduateUrl, graduateText }) => {
+  const data: ShareData = {
+    title: APP_NAME,
+    url: graduateUrl,
+    text: graduateText,
+  };
+
+  const hasWebShare = !!navigator && !!navigator.share;
+  const canShareData = hasWebShare && !!navigator.canShare?.(data);
+
+  function onShare() {
+    navigator.share(data);
+  }
+
+  if (!canShareData) return null;
+  return (
+    <S.ImageToolbarIconButton onClick={onShare}>
+      <Share />
+    </S.ImageToolbarIconButton>
+  );
+};
+
+export default ShareButton;
