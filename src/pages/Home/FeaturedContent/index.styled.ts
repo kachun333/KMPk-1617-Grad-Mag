7e6c7495bc7c74a4f6af60bdc26492f4bc49325f
kachun333 <1998chun@gmail.com>
2023-11-27 20:54:49 +0800

    feat: image hover overlay

--- src/pages/Home/FeaturedContent/index.styled.ts
+++ src/pages/Home/FeaturedContent/index.styled.ts
@@ -2,12 +2,19 @@ import { CardActionArea } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import BannerImage from "components/common/BannerImage";
 
-export const StyledCardActionArea = styled(CardActionArea)({
+export const StyledCardActionArea = styled(CardActionArea)(({ theme }) => ({
   width: "100%",
   aspectRatio: "3 / 2",
-});
+  "&:hover": {
+    background: `linear-gradient(to right, ${theme.palette.secondary.light}, ${theme.palette.primary.light})`,
+  },
+}));
 
-export const StyledCardImage = styled(BannerImage)({
+export const StyledCardImage = styled(BannerImage)(() => ({
   width: "100%",
   height: "100%",
-});
+  opacity: 1,
+  "&:hover": {
+    opacity: 0.95
+  },
+}));
